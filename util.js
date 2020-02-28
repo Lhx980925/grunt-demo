42bb549a6b11abd36e7bf657399d11630ea9a4fc
lihanxiao <1044177723@qq.com>
2020-02-28 09:02:46 +0800

    add 06

---
+++ util.js
@@ -0,0 +1,10 @@
+function rectangle(){
+  return {
+    'perimeter':function(width,height){
+      return (width+height)*2;
+    },
+    'area':function(width,height){
+      return width*height;
+    }
+  }
+}
